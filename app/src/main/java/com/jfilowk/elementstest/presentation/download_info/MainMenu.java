74721d80a7064d9502c5ac7f43db05bb104bafe2
Javier García <javier.tb@outlook.com>
2016-04-30 16:07:32 +0200

    OnClick event

--- app/src/main/java/com/jfilowk/elementstest/presentation/download_info/MainMenu.java
+++ app/src/main/java/com/jfilowk/elementstest/presentation/download_info/MainMenu.java
@@ -1,14 +1,27 @@
 package com.jfilowk.elementstest.presentation.download_info;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import com.jfilowk.elementstest.R;
 
 public class MainMenu extends AppCompatActivity {
 
+  @BindView(R.id.btn_show_me) Button btnShowList;
+
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main_menu);
+
+    ButterKnife.bind(this);
+  }
+
+  @OnClick(R.id.btn_show_me)
+  public void navigateToList(){
+    // TODO: implement navigation to
   }
 
   @Override protected void onResume() {
